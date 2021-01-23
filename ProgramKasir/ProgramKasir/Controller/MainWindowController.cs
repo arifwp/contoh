d0b3b1ea2e496b9b3ebb17c0e1adedfb613ca3d6
arifwp <52806402+arifwp@users.noreply.github.com>
2021-01-23 14:08:15 +0700

    Add files via upload

---
+++ ProgramKasir/ProgramKasir/Controller/MainWindowController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Toko.Model;
+
+namespace Toko.Controller
+{
+
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+        Payment payment;
+
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja, Payment payment)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+            this.payment = payment;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+
+        public void removeItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void addDiskon(Diskon diskon)
+        {
+
+            this.payment.addDiskon(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.payment.getDiskon();
+        }
+
+
+    }
+}
---
+++ ProgramKasir/ProgramKasir/Model/Payment.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toko.Model
+{
+    class Payment
+    {
+        private double balance = 0;
+        public List<Diskon> diskonDipakai;
+        OnPaymentChangedListener paymentListener;
+        public Payment(OnPaymentChangedListener paymentListener)
+        {
+
+            this.paymentListener = paymentListener;
+            this.diskonDipakai = new List<Diskon>();
+
+
+
+        }
+        public List<Diskon> getDiskon()
+        {
+            return this.diskonDipakai;
+        }
+
+        public void setBalance(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double getBalance()
+        {
+            return this.balance;
+        }
+
+
+
+        public void addDiskon(Diskon diskon)
+        {
+            this.diskonDipakai.Clear();
+            this.diskonDipakai.Add(diskon);
+            this.paymentListener.addPromoSucceed();
+        }
+
+
+
+        public void updateTotal(double subTotal)
+        {
+            double promo = 0;
+
+            foreach (Diskon diskon in diskonDipakai)
+            {
+                if (diskon.potongan == 10000)
+                {
+                    promo = 10000;
+                }
+                else if (diskon.potongan == 30000)
+                {
+
+                    promo = (subTotal * 30 / 100);
+
+                    if (promo > 30000)
+                    {
+                        promo = 30000;
+                    }
+                    else
+                    {
+                        promo = (subTotal * 30 / 100);
+                    }
+
+                }
+                else if (diskon.potongan == 25000)
+                {
+                    promo = (subTotal * 25 / 100);
+
+                }
+
+            }
+
+            double total = subTotal - promo;
+            this.paymentListener.onPriceUpdated(subTotal, total);
+
+        }
+    }
+
+    interface OnPaymentChangedListener
+    {
+        void onPriceUpdated(double subtTotal, double total);
+
+        void addPromoSucceed();
+    }
+}
